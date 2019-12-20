7c091fcf52304a10bab673ef2b442ec0a9f0b34b
Chris Engel <Chris.Engel@yahoo.com>
2019-12-20 14:26:42 -0500

    MVP done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Switch, Route, Link } from 'react-router-dom';
+import Welcome from './components/WelcomePage';
+import Characters from './components/CharacterList';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Switch>
+        
+        <Route path='/characters'>
+          <Characters />
+        </Route>
+
+        <Route path='/'>
+          <Welcome />
+        </Route>
+
+      </Switch>
+
     </main>
   );
 }
